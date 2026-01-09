d782ef1106c2ef08d4cdfa3082219640372f6eac
Rahul Gowda <70683277+RahulGowdaEL@users.noreply.github.com>
2026-01-09 22:26:15 +0530

    Update ral.sv

--- ral.sv
+++ ral.sv
@@ -1,3 +1,114 @@
+//explicit pred
+////////////////////////////////////////////////////////////////////////
+///////////////////////reg adapter
+ 
+class top_adapter extends uvm_reg_adapter;
+  `uvm_object_utils (top_adapter)
+ 
+  //---------------------------------------
+  // Constructor 
+  //--------------------------------------- 
+  function new (string name = "top_adapter");
+      super.new (name);
+   endfunction
+  
+  //---------------------------------------
+  // reg2bus method 
+  //--------------------------------------- 
+  function uvm_sequence_item reg2bus(const ref uvm_reg_bus_op rw);
+    transaction tr;    
+    tr = transaction::type_id::create("tr");
+    
+    tr.wr    = (rw.kind == UVM_WRITE);
+    tr.addr  = rw.addr;
+    
+    if(tr.wr == 1'b1) tr.din = rw.data;
+    
+    return tr;
+  endfunction
+ 
+  //---------------------------------------
+  // bus2reg method 
+  //--------------------------------------- 
+  function void bus2reg(uvm_sequence_item bus_item, ref uvm_reg_bus_op rw);
+    transaction tr;
+    
+    assert($cast(tr, bus_item));
+ 
+    rw.kind = (tr.wr == 1'b1) ? UVM_WRITE : UVM_READ;
+    rw.data = (tr.wr == 1'b1) ? tr.din : tr.dout;///rw.data = tr.dout;
+    rw.addr = tr.addr;
+    rw.status = UVM_IS_OK;
+  endfunction
+endclass
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+////////////////////////////////////////////////////////////////////////
+ 
+class env extends uvm_env;
+ `uvm_component_utils(env)
+  
+  agent          agent_inst;
+  top_reg_block  regmodel;   
+  top_adapter    adapter_inst;
+  uvm_reg_predictor   #(transaction)  predictor_inst;
+  
+  sco s;
+  
+  
+  //--------------------------------------- 
+  // constructor
+  //---------------------------------------
+  function new(string name = "env", uvm_component parent);
+    super.new(name, parent);
+  endfunction : new
+ 
+  //---------------------------------------
+  // build_phase - create the components
+  //---------------------------------------
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    predictor_inst = uvm_reg_predictor#(transaction)::type_id::create("predictor_inst", this);
+    agent_inst = agent::type_id::create("agent_inst", this);
+    s = sco::type_id::create("s", this);
+    regmodel   = top_reg_block::type_id::create("regmodel", this);
+    regmodel.build();
+    
+    
+    adapter_inst = top_adapter::type_id::create("adapter_inst",, get_full_name());
+    
+ 
+  endfunction 
+  
+ 
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    
+    agent_inst.m.mon_ap.connect(s.recv);
+     
+    regmodel.default_map.set_sequencer( .sequencer(agent_inst.seqr), .adapter(adapter_inst) );
+    regmodel.default_map.set_base_addr(0);
+    
+    
+    
+    predictor_inst.map       = regmodel.default_map;
+    predictor_inst.adapter   = adapter_inst;
+    agent_inst.m.mon_ap.connect(predictor_inst.bus_in);
+   
+    
+    regmodel.default_map.set_auto_predict(0);
+  endfunction 
+ 
+endclass
+
+
+///implicit prediction
+
 import uvm_pkg::*;
 `include "uvm_macros.svh"
  
